2c4afcceb3c2999788039bb1faa3ca8c6604d744
alternativeTime <matthew.campbell1980@gmail.com>
2015-03-09 19:30:40 +1100

    testing units

---
+++ glycoSpectrumScan/src/test/GssTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import unicarb.ProteinSequenceUni;
+
+/**
+ * Created by matthew on 03/03/15.
+ */
+public class GssTest {
+
+    @Test
+    void ProteinSequenceTest() {
+        ProteinSequenceUni.getSequence("P01833");
+    }
+}
